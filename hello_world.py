1abd523ab441dc1809211d64723b79b42565444c
m31 <14306799+m-31@users.noreply.github.com>
2019-10-18 14:26:21 +0200

    working on mac

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import tkinter as tk
+#import tkinter as tk
+from tkinter import ttk
 
-class Application(tk.Frame):
+class Application(ttk.Frame):
 
     def __init__(self, master=None):
         super(Application, self).__init__(master)
@@ -9,11 +10,16 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.mondialLabel = tk.Label(self, text='Hello World')
-        self.mondialLabel.config(bg="#00ffff")
+        self.mondialLabel = ttk.Label(self, text='Hello World')
         self.mondialLabel.grid()
-        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-        self.quitButton.grid()
+        style = ttk.Style()
+        style.map("C.TButton",
+                  foreground=[('pressed', 'red'), ('active', 'blue')],
+                  background=[('pressed', '!disabled', 'black'),
+                              ('active', 'white')]
+                  )
+        self.quitButton = ttk.Button(self, text='Quit', command=self.quit, style='C.TButton')
+        self.quitButton.grid(row=2, column=2, sticky='w')
 
 app = Application()
 app.master.title('Sample application')
